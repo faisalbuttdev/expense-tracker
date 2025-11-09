41bfe70a69f49bb34bdff129fcdcc261011c3fdb
faisalbuttdev <faisalbutt.dev@gmail.com>
2025-11-10 01:48:31 +0500

    Tried to add the required flags for the project

--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 )
 
 func main() {
-	add := flag.String("add", "foo", "a string")
-	list := flag.String("list", "foo", "a string")
-	delete := flag.String("delete", "foo", "a string")
-	summary := flag.String("summary", "foo", "a string")
-	amount := flag.String("amount ", "foo", "a string")
-	id := flag.String("id  ", "foo", "a string")
+	add := flag.Bool("add", false, "a bool")
+	//list := flag.String("list", "foo", "a string")
+	//delete := flag.String("delete", "foo", "a string")
+	//summary := flag.String("summary", "foo", "a string")
+	amount := flag.Int("amount", 0, "a integer")
 
+	//id := flag.String("id  ", "foo", "a string")
+	description := flag.String("description", "foo", "a string")
 	flag.Parse()
-	fmt.Println(*add, *list, *delete, *summary, *amount, *id)
+	if *add {
+		fmt.Println(*description, *amount)
+	}
+
+	//fmt.Println(*add, *list, *delete, *summary, *amount, *id)
+	//Hello
 
 }
